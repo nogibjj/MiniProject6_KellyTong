f8ab6371c1620a9b31807070f2f1b600aefba7e7
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-09 17:05:55 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,9 +1,59 @@
-#this is for testing functions in main.py
+"""
+Test goes here
 
-from main import subtraction
+"""
 
-def test_subtraction():
-    "testing the subtraction function in main.py"
-    assert subtraction(5,5) == 0
-    assert subtraction (1,6) == -5
-    assert subtraction (6,1) == 5
+import subprocess
+
+
+def test_extract():
+    """tests extract()"""
+    result = subprocess.run(
+        ["python", "main.py", "extract"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+    assert "Extracting data..." in result.stdout
+
+
+def test_transform_load():
+    """tests transfrom_load"""
+    result = subprocess.run(
+        ["python", "main.py", "transform_load"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+    assert "Transforming data..." in result.stdout
+
+
+def test_general_query():
+    """tests general_query"""
+    result = subprocess.run(
+        [
+            "python",
+            "main.py",
+            "general_query",
+            """SELECT t1.server, t1.opponent,
+                AVG(t1.seconds_before_next_point) as avg_seconds_before_next_point,
+                COUNT(*) as total_matches_played
+            FROM default.grad-studentsdb t1
+            JOIN default.all-agesdb t2 ON t1.id = t2.id
+            GROUP BY t1.server, t1.opponent
+            ORDER BY total_matches_played DESC
+            LIMIT 10""",
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+
+
+if __name__ == "__main__":
+    test_extract()
+    test_transform_load()
+    test_general_query()
