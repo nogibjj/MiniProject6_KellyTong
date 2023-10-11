08a6f5a7f567a7c43b96bf95b7441abc8b00aee4
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-10 23:13:53 -0400

    Update transform_load.py

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 
 # load the csv file and insert into databricks
-def load(dataset="data/grad_students.csv", dataset2="data/all_ages.csv"):
+def load(dataset="data/women_stem.csv", dataset2="data/all_ages.csv"):
     """Transforms and Loads data into the local databricks database"""
     df = pd.read_csv(dataset, delimiter=",", skiprows=1)
     df2 = pd.read_csv(dataset2, delimiter=",", skiprows=1)
@@ -25,7 +25,7 @@ def load(dataset="data/grad_students.csv", dataset2="data/all_ages.csv"):
     ) as connection:
         c = connection.cursor()
         # INSERT TAKES TOO LONG
-        # c.execute("DROP TABLE IF EXISTS grad_studentsDB")
+        # c.execute("DROP TABLE IF EXISTS women_stemDB")
         c.execute("SHOW TABLES FROM default LIKE 'serve*'")
         result = c.fetchall()
         # takes too long so not dropping anymore
@@ -33,29 +33,16 @@ def load(dataset="data/grad_students.csv", dataset2="data/all_ages.csv"):
         if not result:
             c.execute(
                 """
-                CREATE TABLE IF NOT EXISTS grad_studentsDB (
+                CREATE TABLE IF NOT EXISTS women_stemDB (
+                    Rank int, 
                     Major_code int,
                     Major string,
                     Major_category string,
-                    Grad_total int,
-                    Grad_sample_size int,
-                    Grad_employed int,
-                    Grad_full_time_year_round int, 
-                    Grad_unemployed int,
-                    Grad_unemployment_rate float, 
-                    Grad_median int,
-                    Grad_P25 int,
-                    Grad_P75 int, 
-                    Nongrad_total int,
-                    Nongrad_employed int,
-                    Nongrad_full_time_year_round int, 
-                    Nongrad_unemployed int, 
-                    Nongrad_unemployment_rate float, 
-                    Nongrad_median int, 
-                    Nongrad_P25 int, 
-                    Nongrad_P75 int, 
-                    Grad_share float, 
-                    Grad_premium float
+                    Total int,
+                    Men int, 
+                    Women int, 
+                    ShareWomen int, 
+                    Median int
                 )
             """
             )
@@ -63,12 +50,12 @@ def load(dataset="data/grad_students.csv", dataset2="data/all_ages.csv"):
             for _, row in df.iterrows():
                 convert = (_,) + tuple(row)
                 try:
-                    c.execute(f"INSERT INTO grad_studentsDB VALUES {convert}")
+                    c.execute(f"INSERT INTO women_stemDB VALUES {convert}")
                 except Exception as e:
                     print(f"Problematic row: {convert}")
                     print(f"Exception: {e}")
                     break  # Stop after the first error for easier debugging
-                #c.execute(f"INSERT INTO grad_studentsDB VALUES {convert}")
+                #c.execute(f"INSERT INTO women_stemDB VALUES {convert}")
         c.execute("SHOW TABLES FROM default LIKE 'event*'")
         result = c.fetchall()
         # c.execute("DROP TABLE IF EXISTS all_agesDB")
